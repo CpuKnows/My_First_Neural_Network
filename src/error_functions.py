Fixed backpropagation and changed layer structure
Sigmoid function wasn't backpropagating properly.

src/error_functions.py
@@ -7,20 +7,27 @@ tldr:   Functions for measuring error
 import numpy as np
 
 
-def accuracy(predicted, actual, top=1, stratified=False):
+def accuracy(predicted, actual, stratified=False, num_classes=2, top=1):
+    '''
     correct = 0.
-    correct_num = [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]
-    total_num = [actual.count(i) for i in range(10)]
+    correct_num = [0.] * num_classes
+    if stratified is True:
+        total_num = [actual.count(i) for i in range(10)]
 
     for a_i, p_i in zip(actual, predicted):
         if a_i in p_i[0:top]:
             correct += 1
             correct_num[a_i] += 1
 
-    if stratified is False:
-        return correct / len(actual)
-    else:
+    if stratified is True:
         return {i: n / d for n, d, i in zip(correct_num, total_num, range(10))}
+    else:
+        return correct / len(actual)
+    '''
+
+    predicted_index = np.argmax(predicted, axis=1)
+    accuracy_percent = np.sum(actual[np.arange(len(actual)), predicted_index]) / predicted.shape[0]
+    return accuracy_percent
 
 
 def logloss(predicted, actual):

src/layer.py
@@ -10,18 +10,19 @@ import numpy as np
 
 class Layer(object):
 
-    def __init__(self, layer_size, below=None, above=None):
+    def __init__(self, layer_size, layer_type, below=None, above=None):
         """
         :param layer_size: number of nodes in layer
         :param below: object of class layer below in network
         :param above: object of class layer above in network
         """
         self.layer_size = layer_size
-        self.node_values = None
-        self.weights = None
+        self.layer_type = layer_type
         self.below = below
         self.above = above
-        self.dropout = False
+        self.weights = None
+        self.signal_values = None
+        self.activation_values = None
 
     def forward_prop(self, x):
         """
@@ -33,7 +34,9 @@ class Layer(object):
         :return: forward propagation of this layer
         """
 
-        return np.dot(self.node_values, self.weights)
+        self.signal_values = x
+        self.activation_values = self.signal_values
+        return np.dot(self.activation_values, self.weights)
 
     def backward_prop(self, layer_delta):
         """
@@ -43,7 +46,10 @@ class Layer(object):
         :return: error and gradient change of this layer
         """
 
-        layer_error = np.dot(layer_delta, self.weights.T)
+        if self.layer_type is 'output':
+            layer_error = layer_delta - self.activation_values
+        else:
+            layer_error = np.dot(layer_delta, self.weights.T)
         layer_delta = layer_error
 
         return layer_error, layer_delta

src/layers/input_layer.py
@@ -12,7 +12,7 @@ from layer import Layer
 
 class InputLayer(Layer):
 
-    def __init__(self, layer_size, below=None, above=None):
+    def __init__(self, layer_size, layer_type, below=None, above=None):
         """
         :param layer_size: number of nodes in layer
         :param below: object of class layer below in network
@@ -20,11 +20,12 @@ class InputLayer(Layer):
         """
         super(Layer, self).__init__()
         self.layer_size = layer_size
+        self.layer_type = layer_type
         self.below = below
         self.above = above
-        self.node_values = None
         self.weights = None
-        self.dropout = False
+        self.signal_values = None
+        self.activation_values = None
 
     def forward_prop(self, x):
         """
@@ -34,8 +35,8 @@ class InputLayer(Layer):
         :return: forward propagation of this layer
         """
 
-        self.node_values = x
-        return np.dot(self.node_values, self.weights)
+        self.activation_values = x
+        return np.dot(self.activation_values, self.weights)
 
     def backward_prop(self, layer_delta):
         """

src/layers/sigmoid_layer.py
@@ -12,7 +12,7 @@ from layer import Layer
 
 class SigmoidLayer(Layer):
 
-    def __init__(self, layer_size, below=None, above=None):
+    def __init__(self, layer_size, layer_type, below=None, above=None):
         """
         :param layer_size: number of nodes in layer
         :param below: object of class layer below in network
@@ -20,11 +20,12 @@ class SigmoidLayer(Layer):
         """
         super(Layer, self).__init__()
         self.layer_size = layer_size
+        self.layer_type = layer_type
         self.below = below
         self.above = above
-        self.node_values = None
         self.weights = None
-        self.dropout = False
+        self.signal_values = None
+        self.activation_values = None
 
     def forward_prop(self, x):
         """
@@ -34,8 +35,12 @@ class SigmoidLayer(Layer):
         :return: forward propagation of this layer
         """
 
-        self.node_values = self.sigmoid(x)
-        return np.dot(self.node_values, self.weights)
+        self.signal_values = x
+        self.activation_values = self.sigmoid(x)
+        if self.layer_type is not 'output':
+            return np.dot(self.activation_values, self.weights)
+        else:
+            return self.activation_values
 
     def backward_prop(self, layer_delta):
         """
@@ -45,8 +50,11 @@ class SigmoidLayer(Layer):
         :return: error and gradient change of this layer
         """
 
-        layer_error = np.dot(layer_delta, self.weights.T)
-        layer_delta = layer_error * self.sigmoid_prime(self.node_values)
+        if self.layer_type is 'output':
+            layer_error = self.activation_values - layer_delta
+        else:
+            layer_error = np.dot(layer_delta, self.weights.T)
+        layer_delta = layer_error * self.sigmoid_prime(self.signal_values)
 
         return layer_error, layer_delta
 

src/layers/softmax_logloss_layer.py
@@ -12,7 +12,7 @@ from layer import Layer
 
 class SoftmaxLoglossLayer(Layer):
 
-    def __init__(self, layer_size, below=None, above=None):
+    def __init__(self, layer_size, layer_type, below=None, above=None):
         """
         :param layer_size: number of nodes in layer
         :param below: object of class layer below in network
@@ -20,11 +20,12 @@ class SoftmaxLoglossLayer(Layer):
         """
         super(Layer, self).__init__()
         self.layer_size = layer_size
+        self.layer_type = layer_type
         self.below = below
         self.above = above
-        self.node_values = None
         self.weights = None
-        self.dropout = False
+        self.signal_values = None
+        self.activation_values = None
 
     def forward_prop(self, x):
         """
@@ -34,8 +35,9 @@ class SoftmaxLoglossLayer(Layer):
         :return: forward propagation of this layer
         """
 
-        self.node_values = self.softmax(x)
-        return self.node_values
+        self.signal_values = x
+        self.activation_values = self.softmax(self.signal_values)
+        return self.activation_values
 
     def backward_prop(self, layer_delta):
         """
@@ -46,9 +48,10 @@ class SoftmaxLoglossLayer(Layer):
         """
 
         y_index = np.argmax(layer_delta, axis=1)
-        layer_error = np.sum([-1 * np.log(i[j]) for i, j in zip(layer_delta, y_index)]) / layer_delta.shape[0]
+        layer_error = np.sum([-1 * np.nan_to_num(np.log(i[j])) for i, j in zip(layer_delta, y_index)]) / \
+                      layer_delta.shape[0]
 
-        temp_delta = np.copy(self.node_values)
+        temp_delta = np.copy(self.activation_values)
         temp_delta[range(layer_delta.shape[0]), np.argmax(layer_delta, axis=1)] -= 1
 
         return layer_error, temp_delta

src/layers/tanh_layer.py
@@ -12,7 +12,7 @@ from layer import Layer
 
 class TanhLayer(Layer):
 
-    def __init__(self, layer_size, below=None, above=None):
+    def __init__(self, layer_size, layer_type, below=None, above=None):
         """
         :param layer_size: number of nodes in layer
         :param below: object of class layer below in network
@@ -20,11 +20,12 @@ class TanhLayer(Layer):
         """
         super(Layer, self).__init__()
         self.layer_size = layer_size
+        self.layer_type = layer_type
         self.below = below
         self.above = above
-        self.node_values = None
         self.weights = None
-        self.dropout = False
+        self.signal_values = None
+        self.activation_values = None
 
     def forward_prop(self, x):
         """
@@ -34,8 +35,9 @@ class TanhLayer(Layer):
         :return: forward propagation of this layer
         """
 
-        self.node_values = self.tanh(x)
-        return np.dot(self.node_values, self.weights)
+        self.signal_values = x
+        self.activation_values = self.tanh(self.signal_values)
+        return np.dot(self.activation_values, self.weights)
 
     def backward_prop(self, layer_delta):
         """
@@ -45,8 +47,11 @@ class TanhLayer(Layer):
         :return: error and gradient change of this layer
         """
 
-        layer_error = np.dot(layer_delta, self.weights.T)
-        layer_delta = layer_error * self.tanh_prime(self.node_values)
+        if self.layer_type is 'output':
+            layer_error = layer_delta - self.activation_values
+        else:
+            layer_error = np.dot(layer_delta, self.weights.T)
+        layer_delta = layer_error * self.tanh_prime(self.signal_values)
 
         return layer_error, layer_delta
 

src/nn_with_adv_layers.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 import time
 import numpy as np
 
+from blob import Blob
 from layer import Layer
 from error_functions import accuracy, logloss
 
@@ -19,6 +20,8 @@ class Network(object):
         """
         self.layers = list()
         self.num_layers = 0
+        self.blobs = list()
+        self.num_blobs = 0
         self.training_error = list()
         self.validation_error = list()
 
@@ -43,6 +46,26 @@ class Network(object):
         self.layers = list()
         self.num_layers = 0
 
+    def initialize_blobs(self):
+        """
+        Initialize blobs to be the appropriate size.
+
+        :return: none
+        """
+
+        for l, l_next in zip(self.layers[:-1], self.layers[1:]):
+            self.blobs.append(Blob(l, l_next))
+            self.num_blobs += 1
+
+    def clear_blobs(self):
+        """
+        remove all blobs
+
+        :return: none
+        """
+        self.blobs = list()
+        self.num_blobs = 0
+
     def initialize_weights(self):
         """
         Initialize weights with mean 0 and range [-1, 1]
@@ -52,6 +75,9 @@ class Network(object):
 
         for l, l_next in zip(self.layers[:-1], self.layers[1:]):
             l.weights = 2 * np.random.random([l.layer_size, l_next.layer_size]) - 1
+
+        #for b in self.blobs:
+        #    b.set_weights(2 * np.random.random(b.shape) - 1)
 
     def iter_batches(self, x, y, batch_size, shuffle=False):
         """
@@ -94,8 +120,8 @@ class Network(object):
         :param print_every: how often to print error
         :return: none
         """
-        training_error = list()
-        validation_error = list()
+        self.training_error = list()
+        self.validation_error = list()
 
         velocity_decay = 0.5
         velocities = [0] * (self.num_layers - 1)
@@ -129,7 +155,7 @@ class Network(object):
 
                 # update weights
                 if momentum:
-                    velocities = [velocity_decay * v - (learning_rate / batch_size) * np.dot(l.node_values.T, d)
+                    velocities = [velocity_decay * v - (learning_rate / batch_size) * np.dot(l.activation_values.T, d)
                                   for v, l, d in zip(velocities, self.layers[:-1], layer_deltas)]
 
                     for l, v in zip(self.layers[:-1], velocities):
@@ -137,7 +163,7 @@ class Network(object):
 
                 else:
                     for l, d in zip(self.layers[:-1], layer_deltas):
-                        l.weights += -1 * learning_rate / batch_size * np.dot(l.node_values.T, d)
+                        l.weights += -1 * learning_rate / batch_size * np.dot(l.activation_values.T, d)
 
             if x_val is not None and y_val is not None:
                 results = self.forward_prop(x)
@@ -147,11 +173,13 @@ class Network(object):
 
             if print_error and epoch % print_every == 0:
                 time_passed = time.clock() - start_time
-                results = self.forward_prop(x)
-                log_loss = logloss(results, y)
-                print('Epoch: %.0f \tLog Loss: %.4f \tElapsed: %.0f s' %
+                results_train = self.forward_prop(x)
+                results_val = self.forward_prop(x_val)
+                #log_loss = logloss(results, y)
+                print('Epoch: %.0f \tTrain Error: %.4f \tValidation Error: %.4f \tElapsed: %.0f s' %
                       (epoch,
-                       log_loss,
+                       accuracy(results_train, y),
+                       accuracy(results_val, y_val),
                        time_passed))
 
         if print_error: